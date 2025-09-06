a021b151f03686a41ce6dcef53e7066e1346b88c
Tabernol <madventurerace@gmail.com>
2025-09-06 10:20:02 +0300

    added ua language

--- src/i18n/I18nConfig.ts
+++ src/i18n/I18nConfig.ts
@@ -1,20 +1,26 @@
 import i18n from 'i18next';
 import en from './en/translation.json';
 import cs from './cs/translation.json';
+import ua from './ua/translation.json';
 import {initReactI18next} from 'react-i18next';
 import LanguageDetector from 'i18next-browser-languagedetector';
 import english_flag from "../assets/images/flags/united-kingdom.svg";
 import cs_flag from "../assets/images/flags/czech-republic.svg";
+import ua_flag from "../assets/images/flags/ukraine.svg"; // ⬅️ new
 
 export const resources = {
     en: {ns1: en,},
-    cs: {ns1: cs,}
+    cs: {ns1: cs,},
+    ua: {ns1: ua,}
 } as const;
 
-export const SUPPORTED_LANGUAGES = ['en','cs'];
+export const SUPPORTED_LANGUAGES = ['en','cs', 'ua'];
 
 export function getLanguageFlagPairFromLocale(locale: string) {
     switch (locale) {
+        case 'uk':
+        case 'ua':
+            return [ua_flag, 'українська'];
         case 'en':
             return [english_flag, 'english'];
         case 'cs':
@@ -27,7 +33,7 @@ export function getLanguageFlagPairFromLocale(locale: string) {
 i18n.use(LanguageDetector)
     .use(initReactI18next)
     .init({
-        fallbackLng: ['en', 'cs'],
+        fallbackLng: ['en', 'cs', 'ua'],
         debug: true,
         interpolation: {
             escapeValue: false,
